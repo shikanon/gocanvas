fc67143ee8f4f5371b64e7e32554633a86562898
shikanon <shikanon@live.com>
2022-05-30 02:48:05 +0800

    fix main.go

--- example/go.mod
+++
@@ -1,5 +0,0 @@
-module exmaple
-
-go 1.16
-
-require github.com/gin-gonic/gin v1.7.7 // indirect
--- example/main.go
+++ example/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	gocanvas "github.com/shikanon/gocanvas/pkg/canvas"
+	"github.com/shikanon/gocanvas/pkg/engine"
 )
 
+func render(w http.ResponseWriter, r *http.Request) {
+	app := gocanvas.NewApplication(w)
+	scene := app.Scene()
+	// 创建一个摄像机
+	camera := gocanvas.DefaultCamera()
+	// 创建一个天空盒
+	skybox := gocanvas.DefaultSkybox()
+	// 创建一个灯光
+	light := gocanvas.DefaultLight()
+	// 创建一个模型
+	model, err := gocanvas.LoadGLTFModel("./assets/corvette_stingray/scene.gltf")
+	if err != nil {
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	model.Pos = engine.Position{
+		X: 0,  // 左右
+		Y: -2, // 上下
+		Z: -5, // 前后
+	}
+	// 加入到场景中
+	scene.AddCamera(camera)
+	scene.AddSkyBox(skybox)
+	scene.AddLight(light)
+	scene.AddModel(model)
+	// 渲染
+	err = scene.Render()
+	if err != nil {
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+}
+
 func main() {
-	r := gin.Default()
-	r.Static("./src", "./src")
-	r.Static("./assets", "./assets")
-	r.GET("/", func(c *gin.Context) {
-		r.LoadHTMLGlob("./*.html")
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	http.HandleFunc("/", render)
+	http.Handle("/src/", http.FileServer(http.Dir("")))
+	http.Handle("/assets/", http.FileServer(http.Dir("")))
+	http.ListenAndServe(":8080", nil)
 }
